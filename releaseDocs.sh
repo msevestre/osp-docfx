ce6511d662405f3a1d03ca1b851a2e95b14f9ef4
Rodolfo Palacios <rpalacios@igloosoftware.com>
2018-02-27 10:40:41 -0500

    Add pdf to the gh-pages branch.

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -19,9 +19,16 @@ git rm -r *
 
 echo "Copy documentation into the repo"
 cp -r $SOURCE_DIR/docfx_project/_site/* .
-cp $SOURCE_DIR/docfx_project/_site_pdf/docfx_project_pdf.pdf ./osp.pdf
+
+echo "Copy pdf into the repo"
+mkdir pdf
+cp $SOURCE_DIR/docfx_project/_site_pdf/docfx_project_pdf.pdf ./pdf/osp.pdf
 
 echo "Push the new docs to the remote branch"
 git add . -A
 git commit -m "Update generated documentation"
 git push origin gh-pages
+
+echo "Cleaning up"
+cd ..
+rm -rf ./docfx-gh-pages
